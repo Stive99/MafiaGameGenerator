b7d3b4eb13f97c4966f7837accdf31876fdc79e1
Stive_99 <aslanych99@mail.ru>
2024-05-16 21:14:02 +0300

    upd

--- MafiaGameGenerator.cpp
+++ MafiaGameGenerator.cpp
@@ -5,73 +5,52 @@
 #include <fstream>
 #include <random>
 #include <ctime>
-#include <map>
 #include <locale>
 #include <codecvt>
+#include "Role.h"
 
-enum Language { English, Russian };
-
-std::map<std::string, std::map<Language, std::string>> translations = {
-	{"Enter the number of players: ", { {English, "Enter the number of players: "}, {Russian, "Введите количество игроков: "} }},
-	{"Number of players should be at least 4.", { {English, "Number of players should be at least 4."}, {Russian, "Количество игроков должно быть не менее 4."} }},
-	{"Enter the names of the players:\n", { {English, "Enter the names of the players:\n"}, {Russian, "Введите имена игроков:\n"} }},
-	{"Player ", { {English, "Player "}, {Russian, "Игрок "} }},
-	{" is ", { {English, " is "}, {Russian, " - "} }},
-	{"Mafia", { {English, "Mafia"}, {Russian, "Мафия"} }},
-	{"Civilian", { {English, "Civilian"}, {Russian, "Мирный"} }},
-	{"Detective", { {English, "Detective"}, {Russian, "Детектив"} }},
-};
-
-Language currentLanguage = Russian; // Change to English if you want to use English
-
-/// <summary>
-/// Translate text to the current language
-/// </summary>
-/// <param name="text"></param>
-/// <returns></returns>
-static std::string translate(const std::string& text) {
-	return translations[text][currentLanguage];
-}
-
-enum Role { Mafia, Civilian, Detective };
-
-std::string roleToString(Role role) {
-	switch (role) {
-		case Mafia: return "Mafia";
-		case Civilian: return "Civilian";
-		case Detective: return "Detective";
-	}
-	return "";
-}
+void assignRoles(int numPlayers, std::vector<Role>& roles);
+void createFilesAndPrintRoles(const std::vector<std::wstring>& players, const std::vector<Role>& roles);
 
 int main() {
-	std::setlocale(LC_ALL, "Russian");
+	// Установить кодировку которая поддерживает кириллицу
+	setlocale(LC_ALL, "");
 
-	// Number of players
+	// Запросить количество игроков
 	int numPlayers;
-	std::cout << translate("Enter the number of players: ");
-	std::cin >> numPlayers;
-
-	// if (numPlayers < 4) {
-	//     std::cerr << "Number of players should be at least 4." << std::endl;
-	//     return 1;
-	// }
-	while(!(std::cin >> numPlayers) || numPlayers < 4) {
-		std::cin.clear(); // clear the error flags
-		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore the rest of the line
-		std::cerr << "Invalid input. Number of players should be a number and at least 4." << std::endl;
-		std::cout << translate("Enter the number of players: ");
+	std::wcout << L"Введите количество игроков: ";
+	while (!(std::wcin >> numPlayers) || numPlayers < 4) {
+		std::wcin.clear();
+		std::wcin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::wcerr << L"Неверный ввод. Количество игроков должно быть числом и не менее 4." << std::endl;
+		std::wcout << L"Введите количество игроков: ";
 	}
 
-	std::vector<std::string> players(numPlayers);
-	std::cout << translate("Enter the number of players: ");
+	// Запросить имена игроков
+	std::vector<std::wstring> players(numPlayers);
+	std::wcout << L"Введите имена игроков:" << std::endl;
+	std::wcin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 	for (int i = 0; i < numPlayers; ++i) {
-		std::cout << translate("Player ") << i + 1 << ": ";
-		std::cin >> players[i];
+		std::wcout << L"Игрок " << i + 1 << L": ";
+		std::getline(std::wcin, players[i]);
 	}
 
-	// Roles assignment
+	// Назначить роли
 	std::vector<Role> roles(numPlayers, Civilian);
+	assignRoles(numPlayers, roles);
+
+	// Создать файлы и вывести роли в консоль
+	createFilesAndPrintRoles(players, roles);
+
+	// Вывести сообщение о завершении
+	std::wcout << L"Файлы созданы." << std::endl;
+	std::wcout << L"Нажмите Enter для выхода...";
+	std::wcin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+	return 0;
+}
+
+void assignRoles(int numPlayers, std::vector<Role>& roles) {
 	int numMafias = numPlayers / 3;
 	int numDetectives = std::max(1, numPlayers / 6);
 
@@ -82,19 +61,30 @@ int main() {
 		roles[i] = Detective;
 	}
 
-	// Shuffle roles
-	std::srand(std::time(0));
-	std::shuffle(roles.begin(), roles.end(), std::mt19937(static_cast<unsigned int>(std::random_device()())));
+	// Перемешать роли
+	std::srand(static_cast<unsigned int>(std::time(0)));
+	std::shuffle(roles.begin(), roles.end(), std::mt19937(std::random_device()()));
+}
 
-	// Output roles and create files
-	for (int i = 0; i < numPlayers; ++i) {
-		std::cout << players[i] << " is " << roleToString(roles[i]) << std::endl;
-		std::ofstream outFile(players[i] + ".txt");
-		outFile << "Player: " << players[i] << "\nRole: " << roleToString(roles[i]) << std::endl;
-		outFile.imbue(std::locale(outFile.getloc(), new std::codecvt_utf8<wchar_t>));  // Set locale to UTF-8
-		outFile.close();
+void createFilesAndPrintRoles(const std::vector<std::wstring>& players, const std::vector<Role>& roles) {
+	if (players.size() != roles.size()) {
+		std::wcerr << L"Ошибка: количество игроков и ролей не совпадает." << std::endl;
+		return;
 	}
 
-	std::cout << "Files created." << std::endl;
-	return 0;
+	for (size_t i = 0; i < players.size(); ++i) {
+		try {
+			std::wofstream outFile(players[i] + L".txt");
+			outFile.imbue(std::locale("Russian_Russia.1251"));
+
+			if (!outFile) {
+				throw std::ios_base::failure("Не удалось открыть файл.");
+			}
+			outFile << L"Игрок: " << players[i] << L"\n" << L"Роль: " << roleToString(roles[i]) << std::endl;
+			outFile.close();
+			std::wcout << L"Игрок: " << players[i] << L"\n" << L"Роль: " << roleToString(roles[i]) << std::endl;
+		} catch (const std::exception& e) {
+			std::wcerr << L"Исключение: " << e.what() << L" для игрока: " << players[i] << std::endl;
+		}
+	}
 }
