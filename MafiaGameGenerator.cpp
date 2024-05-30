1d01b3bc2f8245f4e5fb3e39e82469ec49256ca6
Stive_99 <aslanych99@mail.ru>
2024-05-30 20:50:33 +0300

    Update MafiaGameGenerator.cpp

--- MafiaGameGenerator.cpp
+++ MafiaGameGenerator.cpp
@@ -8,128 +8,244 @@
 #include <locale>
 #include <codecvt>
 #include <climits>
+#include <stdexcept>
+#include <filesystem>
 
 #ifdef _WIN32
-#include <windows.h>
+    #include <windows.h>
+    #include <fcntl.h>
+    #include <io.h>
 #endif
-#include <corecrt_io.h>
-#include <fcntl.h>
+
+using namespace std;
 
 enum Role { Mafia, Civilian, Detective, Doctor, Don };
 
 // Функция для преобразования роли в строку
-std::wstring roleToString(Role role) {
-	switch (role) {
-		case Mafia: return L"Мафия";
-		case Civilian: return L"Мирный";
-		case Detective: return L"Детектив";
-		case Doctor: return L"Доктор";
-		case Don: return L"Мафия (Дон)";
-	}
-	return L"";
+static std::wstring roleToString(Role role) {
+    switch (role) {
+        case Mafia: return L"Мафия";
+        case Civilian: return L"Мирный";
+        case Detective: return L"Детектив";
+        case Doctor: return L"Доктор";
+        case Don: return L"Мафия (Дон)";
+    }
+    return L"";
 }
 
-void assignRoles(int numPlayers, std::vector<Role>& roles) {
-	int numMafias = numPlayers / 3;
-	// std::max(1, numPlayers / 6); | (std::max)(1, numPlayers / 6);
-	int numDetectives = 1;
-	int numDoctors = 1;
-	int numDons = (numMafias > 1) ? 1 : 0;
+// Функция для назначения ролей
+static void assignRoles(int numPlayers, std::vector<Role>& roles) {
+    int numMafias = numPlayers / 3;
+    // std::max(1, numPlayers / 6); | (std::max)(1, numPlayers / 6);
+    int numDetectives = 1;
+    int numDoctors = 1;
+    int numDons = (numMafias > 1) ? 1 : 0;
 
-	for (int i = 0; i < numMafias - numDons; ++i) {
-		roles[i] = Mafia;
-	}
-	if (numDons == 1) {
-		roles[numMafias - 1] = Don;
-	}
-	for (int i = numMafias; i < numMafias + numDetectives; ++i) {
-		roles[i] = Detective;
-	}
-	for (int i = numMafias + numDetectives; i < numMafias + numDetectives + numDoctors; ++i) {
-		roles[i] = Doctor;
-	}
+    for (int i = 0; i < numMafias - numDons; ++i) {
+        roles[i] = Mafia;
+    }
+    if (numDons == 1) {
+        roles[numMafias - 1] = Don;
+    }
+    for (int i = numMafias; i < numMafias + numDetectives; ++i) {
+        roles[i] = Detective;
+    }
+    for (int i = numMafias + numDetectives; i < numMafias + numDetectives + numDoctors; ++i) {
+        roles[i] = Doctor;
+    }
 
-	// Перемешать роли
-	std::random_device rd;
-	std::mt19937 g(rd());
-	std::shuffle(roles.begin(), roles.end(), g);
+    // Перемешать роли
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::shuffle(roles.begin(), roles.end(), g);
 }
 
-void createFilesAndPrintRoles(const std::vector<std::wstring>& players, const std::vector<Role>& roles) {
+// Функция для создания файлов и вывода ролей
+static void createFilesAndPrintRoles(const std::vector<std::wstring>& players, const std::vector<Role>& roles) {
 #ifdef _WIN32
-	SetConsoleOutputCP(CP_UTF8);
+    if (_setmode(_fileno(stdin), _O_WTEXT) == -1) {
+        wcerr << L"Не удалось установить режим для stdin" << endl;
+        return;
+    }
+    if (_setmode(_fileno(stdout), _O_WTEXT) == -1) {
+        wcerr << L"Не удалось установить режим для stdout" << endl;
+        return;
+    }
+    if (_setmode(_fileno(stderr), _O_WTEXT) == -1) {
+        wcerr << L"Не удалось установить режим для stderr" << endl;
+        return;
+    }
+    if (_setmode(_fileno(stdin), _O_U16TEXT) == -1) {
+        wcerr << L"Не удалось установить режим для stdin" << endl;
+        return;
+    }
+    if (_setmode(_fileno(stdout), _O_U16TEXT) == -1) {
+        wcerr << L"Не удалось установить режим для stdout" << endl;
+        return;
+    }
+    if (_setmode(_fileno(stderr), _O_U16TEXT) == -1) {
+        wcerr << L"Не удалось установить режим для stderr" << endl;
+        return;
+    }
 #endif
 
-	for (size_t i = 0; i < players.size(); ++i) {
-		try {
-			std::wstring fileName = players[i] + L".txt";
-			std::wofstream outFile(fileName);
-			if (!outFile) {
-				throw std::ios_base::failure("Ошибка открытия файла");
-			}
-			outFile.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t>));
-			outFile << L"Игрок: " << players[i] << L"\n" << L"Роль: " << roleToString(roles[i]) << std::endl;
-			outFile.close();
-			std::wcout << L"Игрок: " << players[i] << L"\n" << L"Роль: " << roleToString(roles[i]) << std::endl;
-		} catch (const std::exception& e) {
-			std::wcerr << L"Исключение: " << e.what() << L" для игрока: " << players[i] << std::endl;
-			std::wcout << L"Произошла ошибка при создании файла для игрока: " << players[i] << std::endl;
-			std::wcout << L"Продолжить выполнение программы? (y/n): ";
-			wchar_t response;
-			std::wcin >> response;
-			if (response != L'y' && response != L'Y') {
-				std::wcout << L"Программа завершена." << std::endl;
-				return;
-			}
-		}
-	}
+    //std::locale loc(std::locale(), new std::codecvt_utf8_utf16<wchar_t>);
+    std::locale loc("en_US.UTF-8");
+    //std::wcout.imbue(loc);
+    //std::wcin.imbue(loc);
+    //std::wcerr.imbue(loc);
+
+    for (size_t i = 0; i < players.size(); ++i) {
+        try {
+            std::wstring fileName = players[i] + L".txt";
+            std::wofstream outFile(fileName);
+            //outFile.imbue(loc);
+            //outFile.imbue(std::locale(std::locale(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::consume_header>));
+            if (!outFile) {
+                throw std::ios_base::failure("Ошибка открытия файла");
+            }
+            outFile << L"Игрок: " << players[i] << L"\n" << L"Роль: " << roleToString(roles[i]) << std::endl;
+            outFile.close();
+            std::wcout << L"Игрок: " << players[i] << L"\n" << L"Роль: " << roleToString(roles[i]) << std::endl;
+        }
+        catch (const std::exception& e) {
+            std::wcerr << L"Исключение: " << e.what() << L" для игрока: " << players[i] << std::endl;
+            std::wcout << L"Произошла ошибка при создании файла для игрока: " << players[i] << std::endl;
+            std::wcout << L"Продолжить выполнение программы? (y/n): ";
+            wchar_t response;
+            std::wcin >> response;
+            if (response != L'y' && response != L'Y') {
+                std::wcout << L"Программа завершена." << std::endl;
+                return;
+            }
+        }
+    }
+}
+
+// Функция для центрирования окна консоли
+static void centerConsoleWindow() {
+    // Получаем дескриптор окна консоли
+    HWND console = GetConsoleWindow();
+
+    // Получаем текущие размеры окна консоли
+    RECT r;
+    GetWindowRect(console, &r);
+
+    // Получаем размеры экрана
+    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+    // Вычисляем новые координаты для центрирования окна консоли
+    int newPosX = (screenWidth - (r.right - r.left)) / 2;
+    int newPosY = (screenHeight - (r.bottom - r.top)) / 2;
+
+    // Перемещаем окно консоли в центр экрана
+    MoveWindow(console, newPosX, newPosY, r.right - r.left, r.bottom - r.top, TRUE);
 }
 
 int main() {
-	// Установить кодировку, которая поддерживает кириллицу
-	std::setlocale(LC_ALL, "");
+    try {
+        // Установка локали для поддержки кириллицы
+        setlocale(LC_ALL, "");
+
 #ifdef _WIN32
-	SetConsoleOutputCP(CP_UTF8);
+        //_setmode(_fileno(stdin), _O_U16TEXT);
+        //_setmode(_fileno(stdout), _O_U16TEXT);
+        //_setmode(_fileno(stderr), _O_U16TEXT);
+
+        if (_setmode(_fileno(stdin), _O_WTEXT) == -1) {
+            wcerr << L"Не удалось установить режим для stdin" << endl;
+            return 1;
+        }
+        if (_setmode(_fileno(stdout), _O_WTEXT) == -1) {
+            wcerr << L"Не удалось установить режим для stdout" << endl;
+            return 1;
+        }
+        if (_setmode(_fileno(stderr), _O_WTEXT) == -1) {
+            wcerr << L"Не удалось установить режим для stderr" << endl;
+            return 1;
+        }
+        if (_setmode(_fileno(stdin), _O_U16TEXT) == -1) {
+            wcerr << L"Не удалось установить режим для stdin" << endl;
+            return 1;
+        }
+        if (_setmode(_fileno(stdout), _O_U16TEXT) == -1) {
+            wcerr << L"Не удалось установить режим для stdout" << endl;
+            return 1;
+        }
+        if (_setmode(_fileno(stderr), _O_U16TEXT) == -1) {
+            wcerr << L"Не удалось установить режим для stderr" << endl;
+            return 1;
+        }
+
+        // Установить название окна
+        SetConsoleTitle(L"Mafia Game Generator");
+
+        // Центрирование окна консоли
+        centerConsoleWindow();
 #endif
 
-	// Запросить количество игроков
-	int numPlayers;
-	std::wcout << L"Введите количество игроков: ";
-	while (!(std::wcin >> numPlayers) || numPlayers < 4) {
-		std::wcin.clear();
-		std::wcin.ignore(INT_MAX, '\n');
-		std::wcerr << L"Неверный ввод. Количество игроков должно быть числом и не менее 4." << std::endl;
-		std::wcout << L"Введите количество игроков: ";
-	}
+        // Запрос количества игроков
+        int numPlayers;
+        std::wcout << L"Введите количество игроков: ";
+        while (true) {
+            if (!(std::wcin >> numPlayers)) {
+                std::wcin.clear();
+                std::wcin.ignore(INT_MAX, '\n');
+                std::wcerr << L"Неверный ввод. Количество игроков должно быть числом и не менее 4." << std::endl;
+                std::wcout << L"Введите количество игроков: ";
+            }
+            else if (numPlayers < 4) {
+                std::wcerr << L"Неверный ввод. Количество игроков должно быть не менее 4." << std::endl;
+                std::wcout << L"Введите количество игроков: ";
+            }
+            else {
+                std::wcin.ignore(INT_MAX, '\n');
+                break;
+            }
+        }
 
-	// Запросить имена игроков
-	std::vector<std::wstring> players(numPlayers);
-	std::wcout << L"Введите имена игроков:\n";
-	std::wcin.ignore(INT_MAX, '\n');
-	for (int i = 0; i < numPlayers; ++i) {
-		std::wcout << L"Игрок " << i + 1 << L": ";
-		std::getline(std::wcin, players[i]);
-	}
+        // Запрос имен игроков
+        std::vector<std::wstring> players(numPlayers);
+        std::wcout << L"Введите имена игроков:\n";
 
-	std::wcout << L"\n--- Назначение ролей ---\n";
-	std::cout << std::endl;
+        for (int i = 0; i < numPlayers; ++i) {
+            std::wstring playerName;
+            std::wcout << L"Игрок " << i + 1 << L": ";
+            while (true) {
+                std::getline(std::wcin, playerName);
+                if (playerName.empty()) {
+                    std::wcout << L"Имя не может быть пустым.\n";
+                    std::wcout << L"Игрок " << i + 1 << L": ";
+                }
+                else {
+                    players[i] = playerName;
+                    break;
+                }
+            }
+        }
 
-	// Назначить роли
-	std::vector<Role> roles(numPlayers, Civilian);
-	assignRoles(numPlayers, roles);
+        std::wcout << L"\n--- Назначение ролей ---\n";
 
-	// Создать файлы и записать роли
-	createFilesAndPrintRoles(players, roles);
+        // Назначение ролей
+        std::vector<Role> roles(numPlayers, Civilian);
+        assignRoles(numPlayers, roles);
 
-	std::wcout << L"\n---------------\n";
-	std::cout << std::endl;
+        // Создание файлов и вывод ролей
+        createFilesAndPrintRoles(players, roles);
 
-	// Вывести сообщение о завершении
-	std::wcout << L"Файлы созданы." << std::endl;
-	std::cout << std::endl;
-	std::wcout << L"Нажмите Enter для выхода...";
-	std::wcin.ignore(INT_MAX, '\n');
-	std::wcin.get();
+        std::wcout << L"\n---------------\n";
 
-	return 0;
+        // Сообщение о завершении
+        std::wcout << L"Файлы созданы." << std::endl;
+        std::wcout << L"Нажмите Enter для выхода...";
+        std::wcin.get();
+    }
+    catch (const std::exception& e) {
+        std::wcerr << L"Критическая ошибка: " << e.what() << std::endl;
+        std::wcout << L"Нажмите Enter для выхода...";
+        std::wcin.get();
+    }
+
+    return 0;
 }
